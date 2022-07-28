16791995cfacd4667e3b5a362b37b25e3b341e4f
austinlidey <alidey@ewu.edu>
2022-07-27 21:14:56 -0700

    Add Driver file, fix Pandas import

---
+++ driver.py
@@ -0,0 +1,10 @@
+"""
+Property of Austin Lidey, drafted for CSCD-429 DATA MINING
+Created: July 27th, 2022 @ 9:00pm PST
+
+This file consists of the driving code that executes the
+homework assignment.
+"""
+import pandas as pd
+
+print("Running Pandas " + pd.__version__)
