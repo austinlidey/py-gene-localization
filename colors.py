78eb24ba67aa9e1920b1e090fba7ab6829b1de65
austinlidey <alidey@ewu.edu>
2022-07-30 15:31:41 -0700

    Add utility class for terminal text colors

---
+++ colors.py
@@ -0,0 +1,19 @@
+"""
+Property of Austin Lidey, drafted for CSCD-429 DATA MINING
+Created: July 30th, 2022 @ 3:26pm PST 
+
+This class will simply aid in the modification of text output.
+"""
+
+
+class t_colors:
+    """Specific class to enhance terminal text output"""
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
