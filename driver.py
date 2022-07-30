9f957502855203f3533efce3ea6c628f3fae088f
austinlidey <alidey@ewu.edu>
2022-07-30 15:32:57 -0700

    Add methods for data processing, Modify pre-existing data processing

--- driver.py
+++ driver.py
@@ -5,17 +5,43 @@ Created: July 27th, 2022 @ 9:00pm PST
 This file consists of the driving code that executes the
 homework assignment.
 """
+import os
+import sys
 import pandas as pd
-from file_processing import import_data_file
+from file_processing import import_data_file, import_data_files
 
-data_file_paths = [
-    "./gene_files/Genes_relation.data",
-    # "./gene_files/Genes_relation.names", # Data may not be needed for processing
-    "./gene_files/Genes_relation.test",
-    "./gene_files/Interactions_relation.data",
-    # "./gene_files/Interactions_relation.names", # Data may not be needed for processing
-    "./gene_files/Interactions_relation.test"]
+# Flow of Program:
+# - Import data files
+# -- Prep data files
+# --- Apply classifying algorithm
+# ---- Collect data generated
+# ----- Display data above
 
-for data_file in data_file_paths:
-    print("-----------------------------------------------")
-    print(f"{data_file}\n{import_data_file(data_file)}")
+
+def process_input_files(arg_list: list) -> list:
+    """Reads user arguments for file paths, and creates list"""
+    processed_arg_list = []
+    for arg in enumerate(arg_list):
+        processed_arg_list.append(arg[1])
+    # Remove the file execution arg
+    processed_arg_list.reverse()
+    processed_arg_list.pop()
+
+    return processed_arg_list
+
+
+def main():
+    """Driver for data processing and execution"""
+
+    # Process arguments for paths
+    print("LOADING INPUT FILES...")
+    user_file_args = process_input_files(sys.argv)
+
+    # Acquire valid files from paths
+    print("PROCESSING INPUT FILES...")
+    data_files = import_data_files(user_file_args)
+
+
+if __name__ == "__main__":
+    os.system('color')
+    main()
--- file_processing.py
+++ file_processing.py
@@ -2,13 +2,45 @@
 Property of Austin Lidey, drafted for CSCD-429 DATA MINING
 Created: July 30th, 2022 @ 12:00pm PST 
 
-This file will process the input data files for the assignment.
+This class will handle processing of the input data files 
+for the assignment.
 """
 import pandas as pd
+from colors import t_colors
+
+
+def import_data_files(user_file_args: list) -> list:
+    """Collects all valid data files specified in argv.
+        Params:
+            user_file_args: List of user specified paths to data files to process.
+        Returns:
+            list containing pd.DataFrame for each valid data file.
+    """
+    valid_data_files = []
+
+    for file_path in user_file_args:
+        data_file = import_data_file(file_path)
+        if data_file is None:
+            print(
+                f"   {t_colors.FAIL}[FAILURE]{t_colors.ENDC} The file: {file_path} could not be located, skipping...")
+        else:
+            print(
+                f"   {t_colors.OKCYAN}[SUCCESS]{t_colors.ENDC} {file_path} is loaded.")
+            valid_data_files.append(data_file)
+
+    return valid_data_files
 
 
 def import_data_file(file_path: str) -> pd.DataFrame:
-    """ Reads in data from a CSV file"""
-    if file_path is None:
-        raise FileNotFoundError
-    return pd.read_csv(file_path)
+    """Reads in data from a CSV file.
+        Params:
+            file_path: A string representing a single file path
+        Returns:
+            pd.DataFrame containing raw data from data file.
+        Throws:
+            FileNotFoundError: When a file cannot be found from path provided.
+    """
+    try:
+        return pd.read_csv(file_path)
+    except FileNotFoundError:
+        return None
